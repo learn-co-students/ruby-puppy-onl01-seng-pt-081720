0eade49bf9a2c8d70a1ec539f1bab3a177e73276
Oliver Bebber <ogbebber@gmail.com>
2020-09-05 19:29:58 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+  @@all = []
+
+  attr_accessor :name, :save
+
+  def initialize(name)
+    @name= name # <-- instance variable HAS to be "@name" not "@puppy"
+    self.save # <-- self.save grabs each pup name
+    #binding.pry
+  end
+
+  def self.all
+    @@all # <-- returns dog with names
+    #binding.pry
+  end
+
+  def self.clear_all
+    @@all.clear # <-- returns empty []
+    #binding.pry
+  end
+
+  def self.print_all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+    #binding.pry
+  end
+
+  def save
+    @@all << self # <-- adds each pup into array
+    # binding.pry
+  end
+end
