787e7eecbac9e9f7d058514bc132e63a36497834
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-05 13:40:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+class Dog 
+  attr_accessor :name 
+  
+  @@all = []
+   
+  def initialize(name)
+    @name = name
+    save
+    
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.clear_all 
+    @@all.clear
+  end
+  
+  def self.print_all
+    i = 0 
+    while i < @@all.length 
+      puts "#{@@all[i].name}"
+      i += 1 
+    end
+  end
+  
+ def save
+     @@all << self
+   end
+end
